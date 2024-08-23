b981396b839edd7f56f7e0496393b0f651e2f206
Viktor Barczi <Viktor.Barczi1@ibm.com>
2024-08-23 14:28:37 +0200

    security configurations

--- src/main/java/application/security/WebConfig.java
+++ src/main/java/application/security/WebConfig.java
@@ -5,12 +5,11 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-@Configuration
+//@Configuration
 public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:8000")
                 .allowedOrigins("http://localhost:8081")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
